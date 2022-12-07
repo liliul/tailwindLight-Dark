32b5bcd49a9ca157a061d608b36f8e21f652eb37
liliul <liliuestudo@gmail.com>
2022-12-07 18:27:08 -0300

    feature: dark mode salvo no localStorage

--- main.js
+++ main.js
@@ -1,20 +1,39 @@
 let openTheme = document.querySelector('#theme')
 let verI = document.querySelector('#verI')
 
+if (localStorage.theme === 'dark'){document.documentElement.classList.add('dark')}
+
 openTheme.addEventListener('click', () => {
   
   verI.classList.toggle('ph-sun')  
- 
+  
   if (verI.classList.contains('ph-sun')) {
+   
   
     verI.classList.remove('ph-moon')
     document.documentElement.classList.add('dark') 
-  
+    //localStorage.setItem('theme', 'dark');
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      localStorage.theme = 'dark'
+      //document.documentElement.classList.add('dark')
+    
+    }
+
+    // Whenever the user explicitly chooses light mode
+    //localStorage.theme = 'light'
+
+    // Whenever the user explicitly chooses dark mode
+    //localStorage.theme = 'dark'
+    //document.documentElement.classList.add('dark')
+    // Whenever the user explicitly chooses to respect the OS preference
+    //localStorage.removeItem('theme')
+
   }else {
 
     verI.classList.remove('ph-sun')
     verI.classList.add('ph-moon')
     document.documentElement.classList.remove('dark')
+    localStorage.removeItem('theme')
   
   }
 
